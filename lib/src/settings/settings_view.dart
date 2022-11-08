086931da7a5e881cd4c918af24ed94b56aaf6b8d
Simon <simon.mahe@mobiapps.fr>
2022-11-08 15:39:22 +0100

    refactor(settingsView): update page template

--- lib/src/settings/settings_view.dart
+++ lib/src/settings/settings_view.dart
@@ -16,36 +16,56 @@ class SettingsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Settings'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        // Glue the SettingsController to the theme selection DropdownButton.
-        //
-        // When a user selects a theme from the dropdown list, the
-        // SettingsController is updated, which rebuilds the MaterialApp.
-        child: DropdownButton<ThemeMode>(
-          // Read the selected themeMode from the controller
-          value: controller.themeMode,
-          // Call the updateThemeMode method any time the user selects a theme.
-          onChanged: controller.updateThemeMode,
-          items: const [
-            DropdownMenuItem(
-              value: ThemeMode.system,
-              child: Text('System Theme'),
-            ),
-            DropdownMenuItem(
-              value: ThemeMode.light,
-              child: Text('Light Theme'),
-            ),
-            DropdownMenuItem(
-              value: ThemeMode.dark,
-              child: Text('Dark Theme'),
-            )
-          ],
+        appBar: AppBar(
+          title: const Text('Settings'),
         ),
-      ),
-    );
+        body: ListView(
+          children: [
+            ListTile(
+              leading: const Icon(Icons.sunny),
+              title: const Text('Theme'),
+              trailing: DropdownButtonHideUnderline(
+                child: DropdownButton<ThemeMode>(
+                  // Read the selected themeMode from the controller
+                  value: controller.themeMode,
+                  // Call the updateThemeMode method any time the user selects a theme.
+                  onChanged: controller.updateThemeMode,
+                  items: [
+                    DropdownMenuItem(
+                      value: ThemeMode.system,
+                      child: Row(
+                        children: const [
+                          Icon(Icons.phone_android, size: 18),
+                          SizedBox(width: 5),
+                          Text('System Theme'),
+                        ],
+                      ),
+                    ),
+                    DropdownMenuItem(
+                      value: ThemeMode.light,
+                      child: Row(
+                        children: const [
+                          Icon(Icons.sunny, size: 18),
+                          SizedBox(width: 5),
+                          Text('Light Theme'),
+                        ],
+                      ),
+                    ),
+                    DropdownMenuItem(
+                      value: ThemeMode.dark,
+                      child: Row(
+                        children: const [
+                          Icon(Icons.brightness_2, size: 18),
+                          SizedBox(width: 5),
+                          Text('Dark Theme'),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
